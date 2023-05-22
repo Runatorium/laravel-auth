512a0f79d664da77ac66f781f5b0de809241d46c
Runatorium <stefano.alesci1@gmail.com>
2023-05-22 17:48:43 +0200

    Database seeders

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,6 +30,7 @@ Route::middleware(['auth', 'verified'])
             '/',
             [DashboardController::class, 'index']
         )->middleware(['auth', 'verified'])->name('dashboard');
+        route::resource('posts', PostController::class);
     });
 
 
