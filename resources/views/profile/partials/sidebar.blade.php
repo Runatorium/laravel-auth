da32bf821d1e561ae5187e95b1fd339937351ed5
Runatorium <stefano.alesci1@gmail.com>
2023-05-22 17:21:44 +0200

    dashboard rework +routes to admin/

---
+++ resources/views/profile/partials/sidebar.blade.php
@@ -0,0 +1,14 @@
+<a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+</a>
+<hr>
+<ul class="nav nav-pills flex-column mb-auto">
+    <li class="nav-item">
+        <a href="{{ route('admin.dashboard') }}" class="nav-link active" aria-current="page">
+            <svg class="bi pe-none me-2" width="16" height="16">
+                <use xlink:href="#home"></use>
+            </svg>
+            Dashboard
+        </a>
+    </li>
+</ul>
+<hr>
